a7177cd16683400de1092f7cbcad0fbfd9ace4b8
Bijou Kim <bijoukim0805@gmail.com>
2020-12-11 21:25:02 -0800

    firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react'; //import React Component
+import React, { useState, useEffect } from 'react'; //import React Component
 import { Route, Switch, Redirect } from 'react-router';
 import { Link, NavLink } from 'react-router-dom';
 import Select from 'react-select'
 import { AboutPage } from './About';
 import BreedPage from './Breed';
+import FavoritesPage from './Favorites';
 import './App.css'; //import css file!
-import LOGO from './doggy.png'
-// import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import LOGO from './doggy.png';
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import {BsStar} from 'react-icons/bs';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 const SIZES = [
   {label: "Very small", value: "Very small"}, 
@@ -32,33 +36,107 @@ const COLOURS = [
   {label: "Sable", value: "Sable"},
 ];
 
-function App(props) {
+const uiConfig = {
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: true
+    },
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  credentialHelper: 'none',
+  signInFlow: 'popup',
+  callbacks: {
+    //avoid redirects after sign-in
+    signInSuccessWithAuthResult: () => false,
+  },
+};
 
+
+function App(props) {
   const pets = props.dogs; //pretend this was loaded externally or via prop
   const renderDogList = (routerProps) => {
-    return <DogList {...routerProps} pets={pets} />
+    return <DogList {...routerProps} pets={pets} user={user} />
   }
   const renderBreedPage = (routerProps) => {
     return <BreedPage {...routerProps} pets={pets} />
   }
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const[user, setUser] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
 
-  return (
-    <div>
+  //auth state event listener
+  useEffect(() => { //run after conponent loads
+    //listen for changes to the authstate (logged in or not)
+    const authUnregisterFunction = firebase.auth().onAuthStateChanged((firebaseUser) => {
+      if (firebaseUser) {
+        console.log("logged in as " + firebaseUser.displayName);
+        // console.log(firebaseUser);
+        setIsLoading(false);
+        setUser(firebaseUser);
+      } else {
+        console.log("logged out");
+        setUser(null);
+        setIsLoading(false);
+      }
+    })
+
+    return function cleanup() { // what to do when done loading
+      authUnregisterFunction();
+    }
+  }, [])
+
+  const handleSignOut = () => {
+    setErrorMessage(null); //clear any old errors
+    firebase.auth().signOut();
+  }
+  
+  //firebase authenticator
+  // let theAuthenticator = firebase.auth();
+
+  //spinner
+  if (isLoading) {
+    return (
+      <div className="text-center">
+        <i className="fa fa-spinner fa-spin fa-3x" aria-label="Connecting..."></i>
+      </div>
+    )
+  }
+  
+  let content = null; //content to render
+
+  if (!user) {
+    content = (
+      <div className="container">
+        <h1> Sign Up</h1>
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
+      </div>
+    );
+  } else {
+    content = (
+        <div>
       <header className="py-4">
         <div className="container col">
           <h1 className="title"><Link to='/'><img src={LOGO} alt="dog logo" className="logo"/> Care for Paws </Link></h1>
         </div>
         <div>
-          <NavBar/>
+          <NavBar user={user} handleSignOut={handleSignOut}/>      
         </div>
       </header>
      
       <main className="container main-page"> 
+        <div>
+          {errorMessage &&
+            <p className="alert alert-danger">{errorMessage}</p>
+          }
+          {content}
+        </div>
         <div className="row">
           <div className="row">
             <Switch>
               <Route exact path="/" render={renderDogList}/>
               <Route path="/about" component={AboutPage}/>
+              <Route path="/favorites" component={FavoritesPage} dogs={props.dogs}/>
               <Route path="/breed/:breedName" render={renderBreedPage}/>
               <Redirect to="/"/>
             </Switch>
@@ -70,38 +148,40 @@ function App(props) {
         <small>Contact Us: careforpaws@uw.edu</small>
       </footer>
     </div>
-  );
-}
+    )
+  }
 
-function NavBar() {
+  return (
+    <div>
+      {errorMessage &&
+        <p className="alert alert-danger">{errorMessage}</p>
+      }
+      {content}
+    </div>
+  )}
+
+function NavBar(props) {
   return (
     <div className="nav-container">
       <ul id="nav">
         <li><NavLink exact to="/" activeClassName={"activeLink"}>Home</NavLink></li>
         <li><NavLink to="/about" activeClassName={"activeLink"}>About Us</NavLink></li>
+        <li><NavLink to="/favorites" activeClassName={"activeLink"}>Favorites</NavLink></li>
+        <li>
+        {props.user &&
+            <button className="btn btn-warning" onClick={props.handleSignOut}>
+              Log Out {props.user.displayName}
+            </button>} 
+        </li>
       </ul>
     </div>
   );
 }
 
-// function NavBarSecond() {
-//   return(
-//     <Nav className="justify-content-center" activeKey="/home">
-//     <Nav.Item>
-//       <NavLink className="navButtons" exact to="/" activeClassName={"activeLink"}>Home</NavLink>
-//     </Nav.Item>
-//     <Nav.Item>
-//       <NavLink className="navButtons" to="/about" activeClassName={"activeLink"}>About Us</NavLink>
-//     </Nav.Item>
-//   </Nav>
-
-//   );
-// }
-
 function DogList(props) {
   let dogs = props.pets; //handle if not provided a prop
   let dogCards = dogs.map((dog) => {
-    return <DogCard key={dog.BreedName} dog={dog} />;
+    return <DogCard key={dog.BreedName} dog={dog} user={props.user} />;
   })
 
   const [sizes, setSizes] = useState(undefined);
@@ -113,9 +193,13 @@ function DogList(props) {
     dogCards = dogs.map((dog) => {
       for (let i = 0; i < sizes.length; i++) {
         if (dog.Size == sizes[i].value) {
-          return <DogCard key={dog.BreedName} dog={dog} />;
+          return <DogCard key={dog.BreedName} dog={dog} user={props.user} />;
         }
       }
+    })
+  } else {
+    dogCards = dogs.map((dog) => {
+      return <DogCard key={dog.BreedName} dog={dog} user={props.user} />;
     })
   }
 
@@ -130,10 +214,15 @@ function DogList(props) {
       for (let i = 0; i < colours.length; i++) {
         for (let j = 0; j < dog.FurColors.length; j++) {
           if (dog.FurColors[j] == colours[i].value) {
-            return <DogCard key={dog.BreedName} dog={dog} />;
+            return <DogCard key={dog.BreedName} dog={dog} user={props.user} />;
           }
         }
       }
+    })
+  } else {
+      console.log("blah");
+      dogCards = dogs.map((dog) => {
+      return <DogCard key={dog.BreedName} dog={dog} user={props.user} />;
     })
   }
 
@@ -166,23 +255,45 @@ function DogList(props) {
       </div>
     </div>
   );
-
 }
 
 function DogCard(props) {
+  let dog = props.dog; //shortcut
+
   const [redirectTo, setRedirectTo] = useState(undefined);
   const handleClick = () => {
     console.log("You clicked on", props.dog.BreedName);
     setRedirectTo(props.dog.BreedName);
   }
 
+  //post a new chirp to the database
+  const postBreed = (event) => {
+    event.preventDefault(); //don't submit
+    console.log(dog.BreedName);
+    
+    /* TODO: add a new Chirp to the database */
+    // const newBreedObj = {
+    //   breed: dog.BreedName
+    // }
+
+    const newBreedObj = {
+      breed: dog.BreedName,
+      userId: props.user.uid,
+      userName: props.user.displayName
+    }
+
+    const breedsRef = firebase.database().ref('breeds');
+    breedsRef.push(newBreedObj);
+  }
+
   if (redirectTo !== undefined) {
     return <Redirect push to={"/breed/" + redirectTo} />
   }
 
-  let dog = props.dog; //shortcut
+  
   return (
     <div className="card clickable" onClick={handleClick}>
+      <i className="star-icon" key={dog.BreedName} onClick={postBreed}><BsStar/></i>
       <img className="card-img-top" src={dog.images} alt={dog.BreedName} />
       <div className="card-body">
         <p className="card-title">{dog.BreedName} </p>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import DOGS from './Breeds.json';
+import firebase from 'firebase/app';
+import 'firebase/auth'
+import 'firebase/database'
 
 ReactDOM.render(<BrowserRouter><App dogs={DOGS}/></BrowserRouter>, document.getElementById('root')); 
 // we need to add a 'root' section in our html
 // also set the prop element for App
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAEf59xIcwMnVvvheQYc4K6_kbQkz4wmOU",
+    authDomain: "care-for-paws-8d7e4.firebaseapp.com",
+    projectId: "care-for-paws-8d7e4",
+    storageBucket: "care-for-paws-8d7e4.appspot.com",
+    messagingSenderId: "138517543912",
+    appId: "1:138517543912:web:043a2b9aadf3c60825425d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
